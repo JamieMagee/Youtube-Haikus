95239f6aa202f4be1c51675f092117566a41b9f9
Jamie Magee <jamie.magee@gmail.com>
2018-03-18 18:57:43 +0100

    Don't log to file

--- YoutubeHaikus/run.py
+++ YoutubeHaikus/run.py
@@ -28,7 +28,6 @@ time_periods = [
     {'interval': 'year', 'title': 'Top of the year'},
     {'interval': 'all', 'title': 'Top ever'}
 ]
-logging.basicConfig(filename='haikus.log', filemode='w', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 playlists = youtube_helper.playlists()
