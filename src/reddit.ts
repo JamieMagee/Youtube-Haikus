8d08bc2608054e80eda641e93f2d63fc7b925b5e
Jamie Magee <jamie.magee@gmail.com>
2026-05-14 23:28:52 -0700

    Fetch reddit listings anonymously

--- src/reddit.ts
+++ src/reddit.ts
@@ -1,47 +1,23 @@
-import { Properties } from './properties';
 import type { Interval } from './types';
 
 export class Reddit {
-  readonly token: string;
   readonly youtubeRegex =
     /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/;
   readonly limit;
 
   constructor(limit = 50) {
     this.limit = limit;
-    const props = new Properties();
-
-    const options = {
-      method: 'post' as const,
-      payload: {
-        grant_type: 'password',
-        username: props.redditUsername,
-        password: props.redditPassword,
-      },
-      headers: {
-        authorization: `Basic ${Utilities.base64Encode(
-          `${props.redditClientId}:${props.redditClientSecret}`
-        )}`,
-        'user-agent': 'YoutubeHaikus',
-      },
-    };
-    const res = UrlFetchApp.fetch(
-      'https://www.reddit.com/api/v1/access_token',
-      options
-    );
-    this.token = JSON.parse(res.getContentText())['access_token'];
   }
 
   public getTop(interval: Interval): string[] {
     const options = {
       headers: {
         accept: 'application/json',
-        authorization: `Bearer ${this.token}`,
         'user-agent': 'YoutubeHaikus',
       },
     };
     const res = UrlFetchApp.fetch(
-      `https://oauth.reddit.com/r/youtubehaiku/top?t=${interval}&limit=${this.limit}`,
+      `https://www.reddit.com/r/youtubehaiku/top.json?t=${interval}&limit=${this.limit}`,
       options
     );
     const resJson: Listing = JSON.parse(res.getContentText());
